f7a35b8e628fc11e5574aa591b4993e4bcc83607
Trevor Russell <truss.42@gmail.com>
2018-06-13 16:21:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,8 +6,8 @@ def batch_badge_creator(names)
   names.collect {|name| "Hello, my name is #{name}."}
 end
 
-def assign_rooms
-  
+def assign_rooms(names)
+  names.collect.with_index
 end
 
 def printer
